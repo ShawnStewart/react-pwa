dfbe7a79125981176bded2951d0869fc9207b693
Shawn Stewart <shawn.stewart@quarkworks.co>
2024-12-15 19:06:57 -0600

    Init tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import Notifications from '@/sections/Notifications';
 import SW from '@/sections/SW';
 import Sidebar from '@/sections/Sidebar';
 
+import './index.css';
+
 function App() {
   return (
     <Fragment>
